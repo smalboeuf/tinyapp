ff4e22759d5772c4f9c272eb5ec3a7e7e4a93fc7
Sheldon Malboeuf <smalboeuf01@gmail.com>
2020-02-18 22:06:08 +0000

    Created a User Authentication system

--- express_server.js
+++ express_server.js
@@ -29,6 +29,19 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+const users = {
+  "userRandomID": {
+    id: "userRandomID", 
+    email: "user@example.com", 
+    password: "purple-monkey-dinosaur"
+  },
+ "user2RandomID": {
+    id: "user2RandomID", 
+    email: "user2@example.com", 
+    password: "dishwasher-funk"
+  }
+};
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
@@ -38,7 +51,7 @@ app.get("/urls.json", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  let templateVars = {username: req.cookies["username"], urls: urlDatabase };
+  let templateVars = {id: req.cookies["id"], user: users[req.cookies["id"]], urls: urlDatabase };
   res.render('urls_index', templateVars);
   
 });
@@ -51,13 +64,13 @@ app.post("/urls", (req, res) => {
 });
 
 app.get("/urls/new", (req, res) => {
-  templateVars = { username: req.cookies["username"] };
+  templateVars = { id: req.cookies["id"]};
   res.render("urls_new", templateVars);
 });
 
 app.get("/urls/:shortURL", (req, res) => {
   
-  let templateVars = { username: req.cookies["username"], shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL.slice(1)] };
+  let templateVars = { id: req.cookies["id"], user: users[req.cookies["id"]], shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL.slice(1)] };
   
   res.render("urls_show", templateVars);
 });
@@ -70,12 +83,12 @@ app.get("/u/:shortURL", (req, res) => {
   if (req.params.shortURL[0] === ':') {
     
     newURL = urlDatabase[req.params.shortURL.slice(1)];
-    templateVars = { username: req.cookies["username"], shortURL: req.params.shortURL.slice(1), longURL: urlDatabase[req.params.shortURL.slice(1)]};
+    templateVars = { id: req.cookies["id"], user: users[req.cookies["id"]], shortURL: req.params.shortURL.slice(1), longURL: urlDatabase[req.params.shortURL.slice(1)]};
     res.redirect(newURL, templateVars);
   } else {
     
     newURL = urlDatabase[req.params.shortURL];
-    templateVars = { username: req.cookies["username"], shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
+    templateVars = { id: req.cookies["id"], user: users[req.cookies["id"]], shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
     res.redirect(newURL, templateVars);
   }
 
@@ -109,18 +122,83 @@ app.post("/urls/:shortURL/checkEdit", (req, res) => {
   res.redirect("/urls/");
 });
 
+
+
+
+
+
+//Login
+
+app.get("/login", (req, res) => {
+  templateVars = { id: req.cookies["id"] };
+  res.render("signInPage", templateVars);
+});
+
 app.post("/login", (req, res) => {
 
- 
-  //Creating a new cookie
-  res.cookie("username", req.body.username);
+  let accountExists = false;
+
+ for (let i = 0; i < Object.keys(users).length; i++) {
+  //Checking to make sure the email and password match the database
+  if (users[Object.keys(users)[i]].email === req.body.email){
+    accountExists = true;
+    if(req.body.password === users[Object.keys(users)[i]].password) {
+      //Create a cookie
+      res.cookie("id", users[Object.keys(users)[i]].id);
+      break;
+    } else {
+      res.status(403);
+      break;
+    }
+  }
+ }
+
+ if(!accountExists) {
+  res.status(403);
+ }
+ let templateVars = { id: req.cookies["id"], user: users[req.cookies["id"]]}
 
   res.redirect("/urls/");
 });
 
+//When you click the logout button it signs you out by deleting cookie
 app.post("/logout", (req, res) => {
 
-  res.clearCookie("username");
+  res.clearCookie("id");
 
   res.redirect("/urls/")
+});
+
+//Register
+
+app.get("/register", (req, res) => {
+  templateVars = { id: req.cookies["id"], user: users[req.cookies["id"]], };
+  res.render("registrationPage", templateVars);
+});
+
+
+app.post("/register", (req, res) => {
+
+  let copy = false;
+  for (let i = 0; i < Object.keys(users).length; i++) {
+    if (req.body.email === users[Object.keys(users)[i]].email) {
+      copy = true;
+    }
+  }
+
+  if(copy === true) {
+    console.log("That is a copy!");
+    res.status(403);
+  } else { 
+    let newID = generateRandomString();
+
+    users[newID] = {
+      id: newID,
+      email: req.body.email,
+      password: req.body.password
+    }
+  }
+  
+
+  res.redirect("/login");
 });
